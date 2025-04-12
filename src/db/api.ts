7c9bf8a5ea535825d7ecb40108e18db0914bfa24
m.shamsiev <m.shamsiev@yt.uz>
2025-04-12 08:38:22 +0500

    fix: simplify BASE_URL assignment for consistency across environments

--- src/db/api.ts
+++ src/db/api.ts
@@ -1,7 +1,4 @@
-const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 console.log({ BASE_URL });
 
 export const fetchLeaderboardData = async ({
