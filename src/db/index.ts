4ad867da7745d1ab6cd305f59a1cca8836636a7d
m.shamsiev <m.shamsiev@yt.uz>
2025-04-12 08:31:25 +0500

    fix: update database connection environment variables for consistency

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,11 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_USER,
-  port: Number(process.env.DB_PORT),
+  host: process.env.MYSQL_DB_HOST,
+  user: process.env.MYSQL_DB_USER,
+  password: process.env.MYSQL_DB_PASSWORD,
+  database: process.env.MYSQL_DB_USER,
+  port: Number(process.env.MYSQL_DB_PORT),
 });
 
 console.log("env", process.env);
